a5fd457d618d361f74219aff759700a11c4ca890
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2024-03-26 19:14:33 +0530

    Update main.js
    Adding js for scroll-based animation

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ function go_play(){
 function go_open(){
     window.location = 'index3.html'
 }
+
+window.addEventListener('scroll', () => {
+    const text = document.querySelector('.topic');
+    const textPosition = text.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight;
+    if(textPosition < screenPosition){
+      text.classList.add('visible');
+    } else {
+      text.classList.remove('visible');
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const text = document.querySelector('.topic2');
+    const textPosition = text.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight;
+    if(textPosition < screenPosition){
+      text.classList.add('visible');
+    } else {
+      text.classList.remove('visible');
+    }
+});
